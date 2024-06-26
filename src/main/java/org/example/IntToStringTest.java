eb4d1c9c900e100cea683720834e9e8ea8adf8f4
gonnichiwa <keepthefaith00@gmail.com>
2024-06-26 14:57:54 +0900

    upload IntToStringTest

---
+++ src/main/java/org/example/IntToStringTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class IntToStringTest {
+
+    public static void main(String[] args) {
+        int num = Integer.parseInt("123");
+        String sNum = Integer.toString(num);
+        System.out.println(num);
+        System.out.println(sNum);
+    }
+}
---
+++ src/main/java/org/example/p181941.java
@@ -0,0 +1,2 @@
+package org.example;public class p181941 {
+}
