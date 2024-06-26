922314cad6fb31572837821bc2c65de61ecc6b17
jaehunjeong <keepthefaith00@gmail.com>
2024-06-26 15:04:23 +0900

    update IntToString : String.valueOf(num)

--- src/main/java/org/example/IntToStringTest.java
+++ src/main/java/org/example/IntToStringTest.java
@@ -3,9 +3,12 @@ package org.example;
 public class IntToStringTest {
 
     public static void main(String[] args) {
+        // 타입의 변환 (형변환) Int -> String
         int num = Integer.parseInt("123");
         String sNum = Integer.toString(num);
+        String sNum2 = String.valueOf(num);
         System.out.println(num);
         System.out.println(sNum);
+        System.out.println(sNum2);
     }
 }
