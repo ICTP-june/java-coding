3e69066696806f33fd2b5c4e53b7d6d3850290ab
gonnichiwa <keepthefaith00@gmail.com>
2024-06-26 15:58:03 +0900

    문제풀이

--- src/main/java/org/example/p181942.java
+++ src/main/java/org/example/p181942.java
@@ -1,2 +1,36 @@
-package org.example;public class p181942 {
+package org.example;
+
+public class p181942 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/181942
+    public static void main(String[] args) {
+        String str1 = "aaaaa";
+        String str2 = "bbbbb";
+
+        String result = solution(str1, str2);
+        System.out.println(result);
+    }
+
+    public static String solution(String str1, String str2) {
+        String result = "";
+        for(int i = 0; i < str1.length(); i++){
+            result = result.concat(Character.toString(str1.toCharArray()[i]));
+            result = result.concat(Character.toString(str2.toCharArray()[i]));
+        }
+        return result;
+    }
 }
+
+/*
+*
+* 문제 설명
+길이가 같은 두 문자열 str1과 str2가 주어집니다.
+
+두 문자열의 각 문자가 앞에서부터 서로 번갈아가면서 한 번씩 등장하는 문자열을 만들어 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+1 ≤ str1의 길이 = str2의 길이 ≤ 10
+str1과 str2는 알파벳 소문자로 이루어진 문자열입니다.
+입출력 예
+str1	str2	result
+"aaaaa"	"bbbbb"	"ababababab"
+* */
